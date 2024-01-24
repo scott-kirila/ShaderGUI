f04023d9a96b7f5126e4e4496da1d56d10ddb478
Scott Kirila <kirilasj@gmail.com>
2024-01-24 16:13:03 -0500

    Auto-completion working with up/down arrows. Still need to handle left/right arrows and mouse clicks.

--- ShaderGUI/callbacks.cpp
+++ ShaderGUI/callbacks.cpp
@@ -1,5 +1,7 @@
 #include "callbacks.h"
 
+#include "imgui_internal.h"
+
 #include <iostream>
 #include <list>
 #include <string>
@@ -29,6 +31,7 @@ int text_edit_callback(ImGuiInputTextCallbackData* data) {
     static bool s_autocomplete = false;
 
     static int s_linePos{};
+    static int cursorPos = data->CursorPos;
 
     switch (data->EventFlag)
     {
@@ -83,19 +86,11 @@ int text_edit_callback(ImGuiInputTextCallbackData* data) {
     }
     case ImGuiInputTextFlags_CallbackAlways:
     {
-        /*
-        char* temp_line_end = data->Buf + data->CursorPos;
-        char* temp_line_start = temp_line_end;
-        while (temp_line_start > data->Buf) {
-            const char c = temp_line_start[-1];
-            if (c == '\n') break;
-            temp_line_start--;
+        if (data->CursorPos == cursorPos + 1 || data->CursorPos == cursorPos - 1) {
+            cursorPos = data->CursorPos;
         }
-        s_line_pos = (int)(temp_line_end - temp_line_start);
-        */
 
-        // ImGuiIO& io = ImGui::GetIO();
-        const int cursorPos = data->CursorPos;
+        std::cout << cursorPos << "\n";
 
         if (s_candidatesList.size() >= 2) {
 
@@ -103,9 +98,13 @@ int text_edit_callback(ImGuiInputTextCallbackData* data) {
                 ImVec2(ImGui::GetItemRectMax().x - 10 * ImGui::CalcTextSize(" ").x, ImGui::GetItemRectMin().y + ImGui::GetWindowHeight())
             );
             //ImGui::SetNextWindowPos(ImVec2(ImGui::GetItemRectMin().x + ImGui::CalcTextSize(" ").x * (s_line_pos - s_length), ImGui::GetItemRectMax().y));
-
             if (ImGui::BeginTooltip()) {
+
                 s_candidateListOpen = true;
+
+                if  (data->CursorPos > cursorPos +1 || data->CursorPos < cursorPos - 1) {
+                    data->CursorPos = cursorPos;
+                }
 
                 if (ImGui::IsKeyPressed(ImGuiKey_UpArrow)) {
                     s_currentItemIndex--;
@@ -113,6 +112,8 @@ int text_edit_callback(ImGuiInputTextCallbackData* data) {
                 else if (ImGui::IsKeyPressed(ImGuiKey_DownArrow)) {
                     s_currentItemIndex++;
                 }
+
+                cursorPos = data->CursorPos;
 
                 s_currentItemIndex %= s_candidatesList.size();
 
@@ -140,6 +141,9 @@ int text_edit_callback(ImGuiInputTextCallbackData* data) {
                 data->InsertChars(data->CursorPos, " ");
             }
 
+            data->CursorPos = (int)(s_wordStart - data->Buf) + s_candidatesList[s_selectedCandidate].length() + 1;
+            cursorPos = data->CursorPos;
+
             s_wordEnd = nullptr;
             s_wordStart = nullptr;
             s_length = 0;
@@ -153,24 +157,6 @@ int text_edit_callback(ImGuiInputTextCallbackData* data) {
 
         break;
     }
-    case ImGuiInputTextFlags_CallbackCompletion:
-    {
-        if (s_candidatesList.size() >= 1 && s_wordEnd != nullptr && s_wordStart != nullptr)
-        {
-            data->DeleteChars((int)(s_wordStart - data->Buf), s_length);
-            data->InsertChars(data->CursorPos, s_candidatesList[s_selectedCandidate].c_str());
-            data->InsertChars(data->CursorPos, " ");
-        }
-
-        s_wordEnd = nullptr;
-        s_wordStart = nullptr;
-        s_length = 0;
-        s_currentItemIndex = 0;
-        s_candidateListOpen = false;
-        s_candidatesList.clear();
-
-        break;
-    }
     }
 
     return 0;
@@ -179,81 +165,4 @@ int text_edit_callback(ImGuiInputTextCallbackData* data) {
 void glfw_error_callback(int error, const char* description)
 {
 	fprintf(stderr, "GLFW Error %d: %s\n", error, description);
-}
-
-int text_completion_callback(ImGuiInputTextCallbackData* data)
-{
-    std::list<const char*> commands = {
-        "vec2", "vec3", "vec4",
-        "int", "float",
-        "sin", "cos",
-        "texture",
-    };
-
-    ImVector<const char*> Commands;
-
-    for (const auto& command : commands)
-        Commands.push_back(command);
-
-    // Locate beginning of current word
-    const char* word_end = data->Buf + data->CursorPos;
-    const char* word_start = word_end;
-    while (word_start > data->Buf)
-    {
-        const char c = word_start[-1];
-        if (c == ' ' || c == '\t' || c == ',' || c == ';')
-            break;
-        word_start--;
-    }
-
-    // Build a list of candidates
-    ImVector<const char*> candidates;
-    for (int i = 0; i < Commands.Size; i++)
-        if (Strnicmp(Commands[i], word_start, (int)(word_end - word_start)) == 0)
-            candidates.push_back(Commands[i]);
-
-    if (candidates.Size == 0)
-    {
-        // No match
-        //AddLog("No match for \"%.*s\"!\n", (int)(word_end - word_start), word_start);
-    }
-    else if (candidates.Size == 1)
-    {
-        // Single match. Delete the beginning of the word and replace it entirely so we've got nice casing.
-        data->DeleteChars((int)(word_start - data->Buf), (int)(word_end - word_start));
-        data->InsertChars(data->CursorPos, candidates[0]);
-        data->InsertChars(data->CursorPos, " ");
-    }
-    else
-    {
-        // Multiple matches. Complete as much as we can..
-        // So inputing "C"+Tab will complete to "CL" then display "CLEAR" and "CLASSIFY" as matches.
-        int match_len = (int)(word_end - word_start);
-        for (;;)
-        {
-            int c = 0;
-            bool all_candidates_matches = true;
-            for (int i = 0; i < candidates.Size && all_candidates_matches; i++)
-                if (i == 0)
-                    c = toupper(candidates[i][match_len]);
-                else if (c == 0 || c != toupper(candidates[i][match_len]))
-                    all_candidates_matches = false;
-            if (!all_candidates_matches)
-                break;
-            match_len++;
-        }
-
-        if (match_len > 0)
-        {
-            data->DeleteChars((int)(word_start - data->Buf), (int)(word_end - word_start));
-            data->InsertChars(data->CursorPos, candidates[0], candidates[0] + match_len);
-        }
-
-        // List matches
-        //AddLog("Possible matches:\n");
-        //for (int i = 0; i < candidates.Size; i++)
-        //    AddLog("- %s\n", candidates[i]);
-    }
-
-    return 0;
 }
